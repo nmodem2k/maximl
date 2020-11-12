426b1597d9fb8c97bf8c94f68693f4429ae395cb
Modem Naveen <nmodem2k@gmail.com>
2020-11-12 18:03:49 +0530

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,21 @@
+a=input()
+k=[]
+for i in range(0,len(a)):
+  for j in range(i+1,len(a)+1):
+    l=a[i:j]
+    k.append(l) 
+uniquee=list(set(a))
+o=[]
+for i in k:
+  for j in uniquee:
+    if j not in i:
+      break
+  else:
+    o.append(i)
+min_len=len(o[0])
+for i in o:
+  if len(i)<min_len:
+    min_len=len(i)
+
+print(min_len)
+
