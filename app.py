ea6b0b8ef2e7d65bea0d3f841cc98dd3304bac0c
Modem Naveen <nmodem2k@gmail.com>
2020-11-12 17:15:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,11 @@
 
 
 from flask import Flask,render_template,url_for,request
-import pandas as pd 
-import pickle
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
 
-import pickle
+
 
 # load the model from disk
-filename = 'nlp_model.pkl'
-clf = pickle.load(open(filename, 'rb'))
-cv=pickle.load(open('tranform.pkl','rb'))
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -40,8 +34,6 @@ def predict():
             o.append(len(q))
         my_prediction=max(o)  
 
-    #     vect = cv.transform(data).toarray()
-    #     my_prediction = clf.predict(vect)
     return render_template('result.html',prediction = my_prediction)
 
 
