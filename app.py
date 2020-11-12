226d3e99a29cc56dcd4cf9b07a1e3f132de091c8
Modem Naveen <nmodem2k@gmail.com>
2020-11-12 16:51:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+import pickle
+
+# load the model from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv=pickle.load(open('tranform.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+
+    if request.method == 'POST':
+        message = request.form['message']
+        data = message
+        k=[]
+        for i in range(0,len(data)):
+            for j in range(i+1,len(data)+1):
+                l=data[i:j]
+                k.append(l)
+        o=[]
+        for i in k:
+            q=set(i)
+            o.append(len(q))
+        my_prediction=max(o)  
+
+    #     vect = cv.transform(data).toarray()
+    #     my_prediction = clf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
